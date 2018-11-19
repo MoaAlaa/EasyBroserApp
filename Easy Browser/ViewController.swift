5c9e28f0d0e9286ea15bab6302a3da7856f7a699
MoaAlaa <moaalaa16@gmail.com>
2018-11-19 13:30:36 +0200

    Implementing User Choice

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -26,7 +26,26 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
     }
-
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "cancle", style: .cancel, handler: openPage))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
